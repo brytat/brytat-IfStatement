2146e221bc1e293ca1875dec7149b01dd39f94d8
brytat <brytontateishi@gmail.com>
2023-02-16 05:04:54 +0000

    Lab Complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,3 +1,4 @@
+import java.time.YearMonth;
 
 public class Lab {
     /**
@@ -20,6 +21,10 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        return 0;
+        if (bool == true) {
+            return x;
+        } else {
+            return y;
+        }
     }
 }
